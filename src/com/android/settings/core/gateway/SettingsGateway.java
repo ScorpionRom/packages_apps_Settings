LiquidRemix/android_packages_apps_Settings

add activity to launch headsup settings fragment
Signed-off-by: VenkatVishalV <venkatvishal124@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -137,6 +137,7 @@ import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.calling.WifiCallingSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 
+import com.liquid.liquidlounge.fragments.HeadsUpSettings;
 import com.liquid.liquidlounge.LiquidLoungeSettings;
 
 public class SettingsGateway {
@@ -264,6 +265,7 @@ public class SettingsGateway {
             ToggleBackupSettingFragment.class.getName(),
             PreviouslyConnectedDeviceDashboardFragment.class.getName(),
             LiquidLoungeSettings.class.getName(),
+            HeadsUpSettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {

ScorpionRom/packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -139,6 +139,7 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import com.nest.settings.MainActivity;
+import com.nest.settings.ui.HeadsUpSettings;
 
 public class SettingsGateway {
 
@@ -265,7 +266,8 @@ public class SettingsGateway {
             DirectoryAccessDetails.class.getName(),
             ToggleBackupSettingFragment.class.getName(),
             PreviouslyConnectedDeviceDashboardFragment.class.getName(),
-            MainActivity.class.getName()
+            MainActivity.class.getName(),
+            HeadsUpSettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {