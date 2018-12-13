omnirom/android_packages_apps_Settings

[3/3] Settings: add back weather parts
Change-Id: Ie67df05e8981f5fd64dd615b60ee0864c3f3dee8

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -144,6 +144,7 @@ import org.omnirom.omnigears.interfacesettings.BarsSettings;
 import org.omnirom.omnigears.batterylight.BatteryLightSettings;
 import org.omnirom.omnigears.interfacesettings.StyleSettings;
 import org.omnirom.omnigears.moresettings.MoreSettings;
+import org.omnirom.omnigears.moresettings.OmniJawsSettings;
 
 public class SettingsGateway {
 
@@ -278,6 +279,7 @@ public class SettingsGateway {
             GestureSettings.class.getName(),
             BarsSettings.class.getName(),
             BatteryLightSettings.class.getName(),
+            OmniJawsSettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {

src/com/android/settings/search/SearchIndexableResourcesImpl.java
@@ -109,6 +109,7 @@ import org.omnirom.omnigears.interfacesettings.BarsSettings;
 import org.omnirom.omnigears.batterylight.BatteryLightSettings;
 import org.omnirom.omnigears.interfacesettings.StyleSettings;
 import org.omnirom.omnigears.moresettings.MoreSettings;
+import org.omnirom.omnigears.moresettings.OmniJawsSettings;
 
 public class SearchIndexableResourcesImpl implements SearchIndexableResources {
 
@@ -208,6 +209,7 @@ public class SearchIndexableResourcesImpl implements SearchIndexableResources {
         addIndex(org.omnirom.omnigears.GestureSettings.class);
         addIndex(BarsSettings.class);
         addIndex(BatteryLightSettings.class);
+        addIndex(OmniJawsSettings.class);
     }
 
     @Override

ScorpionRom/packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -140,6 +140,7 @@ import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import com.nest.settings.MainActivity;
 import com.nest.settings.ui.HeadsUpSettings;
+import com.nest.settings.moresettings.OmniJawsSettings;
 
 public class SettingsGateway {
 
@@ -267,7 +268,8 @@ public class SettingsGateway {
             ToggleBackupSettingFragment.class.getName(),
             PreviouslyConnectedDeviceDashboardFragment.class.getName(),
             MainActivity.class.getName(),
-            HeadsUpSettings.class.getName()
+            HeadsUpSettings.class.getName(),
+            OmniJawsSettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {

src/com/android/settings/search/SearchIndexableResourcesImpl.java
@@ -97,6 +97,7 @@ import com.android.settings.wallpaper.WallpaperTypeSettings;
 import com.android.settings.wfd.WifiDisplaySettings;
 import com.android.settings.wifi.ConfigureWifiSettings;
 import com.android.settings.wifi.WifiSettings;
+import com.nest.settings.moresettings.OmniJawsSettings;
 
 import java.util.Collection;
 import java.util.HashSet;
@@ -191,6 +192,7 @@ public class SearchIndexableResourcesImpl implements SearchIndexableResources {
         addIndex(RecentLocationRequestSeeAllFragment.class);
         addIndex(PreviouslyConnectedDeviceDashboardFragment.class);
         addIndex(BluetoothDashboardFragment.class);
+        addIndex(OmniJawsSettings.class);
     }
 
     @Override