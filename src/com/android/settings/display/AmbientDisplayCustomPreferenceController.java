DirtyUnicorns/android_packages_apps_Settings

Remove AOSP Ambient display conditionally
The CAF based devices we currently support (OP3/T and Potter) both use a alterative
doze package. Having this under Settings/Display will not only confuse the user
and possibly cause problems.

- Add preference and controller for custom doze package (com.custom.ambient.display)

- Check for potter to launch it's doze package

If potter is not detected, we launch the alterative one found
in our other devices.

Change-Id: Ica57b5c55d9cac473d020923239838bfe7a4dae7

src/com/android/settings/display/AmbientDisplayCustomPreferenceController.java
@@ -1,0 +1,83 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.display;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+import androidx.preference.Preference;
+
+import com.android.internal.util.du.Utils;
+
+import com.android.settings.R;
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settings.overlay.FeatureFactory;
+
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.core.instrumentation.MetricsFeatureProvider;
+
+import static com.android.internal.logging.nano.MetricsProto.MetricsEvent.ACTION_AMBIENT_DISPLAY;
+
+public class AmbientDisplayCustomPreferenceController extends AbstractPreferenceController implements
+        PreferenceControllerMixin {
+
+    private Context mContext;
+
+    private static final String KEY_AMBIENT_DISPLAY_CUSTOM = "ambient_display_custom";
+
+    private final MetricsFeatureProvider mMetricsFeatureProvider;
+
+    public AmbientDisplayCustomPreferenceController(Context context) {
+        super(context);
+
+        mMetricsFeatureProvider = FeatureFactory.getFactory(context).getMetricsFeatureProvider();
+        mContext = context;
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_AMBIENT_DISPLAY_CUSTOM;
+    }
+
+    @Override
+    public boolean handlePreferenceTreeClick(Preference preference) {
+        if (KEY_AMBIENT_DISPLAY_CUSTOM.equals(preference.getKey())) {
+            mMetricsFeatureProvider.action(mContext, ACTION_AMBIENT_DISPLAY);
+            try {
+                Intent intent = new Intent();
+                if (Build.PRODUCT.equals("Moto G5 Plus")) {
+                    intent.setClassName(
+                            "com.dirtyunicorns.settings.device",
+                            "com.dirtyunicorns.settings.device.DozeSettingsActivity");
+                } else {
+                    intent.setClassName(
+                            "com.custom.ambient.display",
+                            "com.custom.ambient.display.DozeSettings");
+                }
+                intent.setAction("com.android.settings.action.EXTRA_SETTINGS");
+                mContext.startActivity(intent);
+            } catch (Exception ignored) {
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return Utils.hasAltAmbientDisplay(mContext.getApplicationContext());
+    }
+}

src/com/android/settings/display/AmbientDisplayNotificationsPreferenceController.java
@@ -25,6 +25,8 @@ import android.text.TextUtils;
 import androidx.annotation.VisibleForTesting;
 import androidx.preference.Preference;
 
+import com.android.internal.util.du.Utils;
+
 import com.android.settings.core.TogglePreferenceController;
 import com.android.settings.overlay.FeatureFactory;
 import com.android.settingslib.core.instrumentation.MetricsFeatureProvider;
@@ -79,7 +81,8 @@ public class AmbientDisplayNotificationsPreferenceController extends
 
     @Override
     public int getAvailabilityStatus() {
-        return getAmbientConfig().pulseOnNotificationAvailable()
+        return !Utils.hasAltAmbientDisplay(mContext.getApplicationContext()) &&
+                getAmbientConfig().pulseOnNotificationAvailable()
                 ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
     }
 

ScorpionRom/packages_apps_Settings

src/com/android/settings/display/AmbientDisplayCustomPreferenceController.java
@@ -1,0 +1,83 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.display;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+import androidx.preference.Preference;
+
+import com.android.internal.util.custom.Utils;
+
+import com.android.settings.R;
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settings.overlay.FeatureFactory;
+
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.core.instrumentation.MetricsFeatureProvider;
+
+import static com.android.internal.logging.nano.MetricsProto.MetricsEvent.ACTION_AMBIENT_DISPLAY;
+
+public class AmbientDisplayCustomPreferenceController extends AbstractPreferenceController implements
+        PreferenceControllerMixin {
+
+    private Context mContext;
+
+    private static final String KEY_AMBIENT_DISPLAY_CUSTOM = "ambient_display_custom";
+
+    private final MetricsFeatureProvider mMetricsFeatureProvider;
+
+    public AmbientDisplayCustomPreferenceController(Context context) {
+        super(context);
+
+        mMetricsFeatureProvider = FeatureFactory.getFactory(context).getMetricsFeatureProvider();
+        mContext = context;
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_AMBIENT_DISPLAY_CUSTOM;
+    }
+
+    @Override
+    public boolean handlePreferenceTreeClick(Preference preference) {
+        if (KEY_AMBIENT_DISPLAY_CUSTOM.equals(preference.getKey())) {
+            mMetricsFeatureProvider.action(mContext, ACTION_AMBIENT_DISPLAY);
+            try {
+                Intent intent = new Intent();
+                if (Build.PRODUCT.equals("Moto G5 Plus")) {
+                    intent.setClassName(
+                            "com.dirtyunicorns.settings.device",
+                            "com.dirtyunicorns.settings.device.DozeSettingsActivity");
+                } else {
+                    intent.setClassName(
+                            "com.custom.ambient.display",
+                            "com.custom.ambient.display.DozeSettings");
+                }
+                intent.setAction("com.android.settings.action.EXTRA_SETTINGS");
+                mContext.startActivity(intent);
+            } catch (Exception ignored) {
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return Utils.hasAltAmbientDisplay(mContext.getApplicationContext());
+    }
+}

src/com/android/settings/display/AmbientDisplayNotificationsPreferenceController.java
@@ -25,6 +25,8 @@ import android.text.TextUtils;
 import androidx.annotation.VisibleForTesting;
 import androidx.preference.Preference;
 
+import com.android.internal.util.custom.Utils;
+
 import com.android.settings.core.TogglePreferenceController;
 import com.android.settings.overlay.FeatureFactory;
 import com.android.settingslib.core.instrumentation.MetricsFeatureProvider;
@@ -79,7 +81,8 @@ public class AmbientDisplayNotificationsPreferenceController extends
 
     @Override
     public int getAvailabilityStatus() {
-        return getAmbientConfig().pulseOnNotificationAvailable()
+        return !Utils.hasAltAmbientDisplay(mContext.getApplicationContext()) &&
+                getAmbientConfig().pulseOnNotificationAvailable()
                 ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
     }
 