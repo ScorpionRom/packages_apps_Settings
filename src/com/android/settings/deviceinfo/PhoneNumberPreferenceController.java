b4bc79cbaddf857bdd6518f13e0b5bd826020795
neporood <neporood@gmail.com>
2020-04-04 15:08:40 -0500

    cleanup: remove phone number from about
    -IF you don't know your own number...
    
    Change-Id: I2d06ded37764a54de4816966410039622f9274d4

--- src/com/android/settings/deviceinfo/PhoneNumberPreferenceController.java
+++ src/com/android/settings/deviceinfo/PhoneNumberPreferenceController.java
@@ -62,7 +62,7 @@ public class PhoneNumberPreferenceController extends BasePreferenceController {
     @Override
     public CharSequence getSummary() {
         //return getFirstPhoneNumber();
-        return mContext.getString(R.string.device_info_protected_single_press);
+        return null;
     }
 
     @Override
@@ -136,7 +136,7 @@ public class PhoneNumberPreferenceController extends BasePreferenceController {
         }
 
         //return getFormattedPhoneNumber(subscriptionInfo);
-        return mContext.getString(R.string.device_info_protected_single_press);
+        return null;
     }
 
     private CharSequence getPreferenceTitle(int simSlot) {
